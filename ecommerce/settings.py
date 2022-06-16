99a4d5e05a98c5f6bd29127477346d8974e65a6c
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-06-16 08:29:23 -0700

    'updatenv'

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = 'django-insecure-cjapu)2*m%rf8=f7#@p2=931619+qm+!ddio57o!b6q1jma+r!'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
