ae7a571ad094189a7c50dcd2f7d95c09e2cb6ffc
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-29 11:37:03 +0630

    update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','kaungkaungecommerce.herokuapp.com']
 # ALLOWED_HOSTS = []
